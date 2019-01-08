6d25ccb2c0b77fa5d5c7839e1e2fd492eb4e92ca
Juliants10 <juliants10@gmail.com>
2019-01-08 17:44:08 -0300

    Init

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace Libreria
+{
+    /* Calculadora de Prueba */
+    public class Calculadora
+    {
+        public long Add(long a, long b)
+        {
+            long result;
+            result = a + b;
+
+            if (result > int.MaxValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else if (result < int.MinValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return (long)result;
+            }
+        }
+            public long Subtract(long a, long b)
+        {
+            long result;
+            result = a - b;
+            if (result > int.MaxValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else if (result < int.MinValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return (long)result;
+            }
+        }
+        public long multiply (long a , long b)
+        {
+            long result;
+            result = a * b;
+            if (result > int.MaxValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else if (result < int.MinValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return (long)result;
+            }
+        }
+        public double divide(double a, double b)
+        {
+            double result;
+            result = a / b;
+            if (b == 0)
+            {
+                throw new System.DivideByZeroException();
+            }
+            else if (result > int.MinValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            else if (result<int.MinValue)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+            {
+                return result;
+            }
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTestCalculador.cs
@@ -0,0 +1,113 @@
+using System;
+using Libreria;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTestCalculadora
+    {
+        [Fact]
+        public void TestCalculatorAdd()
+        {
+            var Calculadora = new Calculadora();
+        }
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(10, 10, 20)]
+        [InlineData(-1, -1, -2)]
+        //[InlineData(2147483647, 2,2147483647)]
+        public void TestCalculadoraAdd(int a, int b, int expected)
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.Add(a, b);
+            Assert.Equal(result, expected);
+        }
+        [Theory]
+        [InlineData(1, 1, 0)]
+        //[InlineData(int.MinValue, 1, int.MinValue)]
+        //[InlineData(int.MaxValue, -1, int.MaxValue)]
+        public void TestCalculadoraSubtract(int a, int b, int expected)
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.Subtract(a, b);
+            Assert.Equal(result, expected);
+        }
+
+        [Theory]
+        [InlineData(6, 4, 24)]
+        //[InlineData(int.MinValue, 2, int.MinValue)]
+        //[InlineData(int.MaxValue, -2, int.MaxValue)]
+        //[InlineData(int.MaxValue, int.MaxValue, 0)]
+        public void TestCalculadoraMultiply(int a, int b,int expected)
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.multiply(a, b);
+            Assert.Equal(result, expected);
+        }
+
+
+        [Theory]
+        [InlineData(8, 4, 2)]
+        //[InlineData(8,0,0)]
+        //[InlineData(Double.MaxValue, 0.5, Double.MaxValue)]
+        //[InlineData(Double.MinValue,0.5,Double.MaxValue)]
+        public void TestCalculadoraDivide(Double a, double b, double expected)
+        {
+            var calculadora = new Calculadora();
+            var result = calculadora.divide(a, b);
+            Assert.Equal(result, expected);
+        }
+        [Fact]
+        public void TestCaculadoraDivideByZero()
+        {
+            var calculadora = new Calculadora();
+            Assert.Throws<DivideByZeroException>(
+                  () =>
+                  {
+                      return calculadora.divide(10, 0);
+                  });
+        }
+        [Fact]
+        public void TestCalculadoraAddEdge()
+        {
+            var calculadora = new Calculadora();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                {
+                    return calculadora.Add(int.MinValue, -1);
+                });
+        }
+        [Fact]
+        public void TestCalculadoraSubtractEdge()
+        {
+            var calculadora = new Calculadora();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                {
+                    return calculadora.Subtract(int.MaxValue, -1);
+                });
+        }
+        [Fact]
+        public void TestCalculadoraMultiplyEdge()
+        {
+            var calculadora = new Calculadora();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                {
+                    return calculadora.multiply(int.MaxValue, 2);
+                });
+        }
+        [Fact]
+        public void TestCalculadoraDivideEdge()
+        {
+            var calculadora = new Calculadora();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                {
+                    return calculadora.divide(int.MaxValue, 0.5);
+                });
+        }
+
+
+    }
+}
